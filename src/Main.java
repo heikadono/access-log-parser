767adb1385c8f377f08cb70a7de187d2367655b2
Tsepenkov Nikita <heikadono@yandex.ru>
2025-05-09 01:53:51 +0500

    added fileReader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -7,18 +9,38 @@ public class Main {
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());*/
 
-        //Курсовая "Циклы"
+
         int correctPartCount = 0;
-        while(true) {
+        while (true) {
             System.out.println("Введите путь к файлу: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.isFile();
-            if(fileExists){
+            if (fileExists) {
                 correctPartCount++;
                 System.out.println("Путь указан верно. Это файл номер " + correctPartCount);
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    String line;
+                    int lineCount=0;
+                    int maxLine = 0;
+                    int minLine = reader.readLine().length();
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if (length>maxLine) maxLine = length;
+                        if (length<minLine) minLine = length;
+                        lineCount++;
+                    }
+                    System.out.println("Общие количество строк: " + lineCount);
+                    System.out.println("Самая длинная строка в файле: " + maxLine);
+                    System.out.println("Самая короткая строка в файле: " + minLine);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             } else {
                 System.out.println("Указанный файл не существует или указанный путь является путём к папке");
+
             }
         }
     }
